3aab801beada4e6493b54f622a64b43dff07dd88
arbindra8833 <50481602+arbindra8833@users.noreply.github.com>
2019-08-12 22:47:48 +0545

    Add files via upload
    first one

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function signup() {
+		document.getElementById('login').style.display="none";
+		document.getElementById('title').innerHTML ='signup Here';
+		document.getElementById('signup').style.display="block";
+	}
+
+function login() {
+		document.getElementById('signup').style.display="none";
+		document.getElementById('title').innerHTML ='Login Here';
+		document.getElementById('login').style.display="block";
+	}
+
